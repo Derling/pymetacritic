fe93c305e5fe09b57a2fc5d14ade86cf9f4797ce
Derling Batista <derling0704@gmail.com>
2017-08-16 09:13:45 -0400

    Added conditionals to improve resource allocation

--- metacritic/PyGameCritic.py
+++ metacritic/PyGameCritic.py
@@ -26,107 +26,152 @@ from bs4 import BeautifulSoup
 #full review body or not, currently we are pulling review body and not doing
 #anything with it, to reduce redundancy and save resources add boolean. 
 class PyGameCritic():
-    def __init__(self, console, game, critics=True, users=True, pool=True):
+    
+    def __init__(self, console, game, critics=True, users=True, pool=True, reviews=False):
         self.user_reviews = {}
         self.critic_reviews = {}
         self.reviews = {}
-        self.get_all_metacritic_data(console, game, critics, users, pool)
-        print(len(self.user_reviews),len(self.critic_reviews))
+        self.get_all_metacritic_data(console, game, critics, users, pool, reviews)
+        self.calculateAvgs()
+        
     #Main method, pool parameter used to determine whether we want all reviews 
-    def get_all_metacritic_data(self, console, game, critics, users, pool): 
+    def get_all_metacritic_data(self, console, game, critics, users, pool, reviews): 
         url = 'http://www.metacritic.com/game/{0}/{1}' \
             .format(console.replace(' ','-'),game.replace(' ','-'))
-        if critics : self.reviews['critics'] = self.get_critic_reviews(url,pool)
-        if users : self.reviews['users'] = self.get_user_reviews(url,pool)
+        if critics : self.reviews['critics'] = self.get_critic_reviews(url, pool, reviews)
+        if users : self.reviews['users'] = self.get_user_reviews(url, pool, reviews)
 
     #get user reviews
-    def get_user_reviews(self, url, pool, page=0):
+    def get_user_reviews(self, url, pool, reviews, page=0):
         req = Request(url + '/user-reviews?page=' + str(page) 
                         , headers={'User-Agent': 'Mozilla/5.0'})
         html_doc = urlopen(req).read()
         soup = BeautifulSoup(html_doc,'lxml')
-        ol = soup.find_all('ol',{'class':'reviews user_reviews'})
+        ol = soup.find('ol',{'class':'reviews user_reviews'})
         user_elements = 0
         if ol:
-            user_elements = ol[0].find_all('li',{'class':'review user_review'})
+            user_elements = ol.find_all('li',{'class':'review user_review'})
         #If statement to check if there is at least one review
         if soup.find('li',{'class':'review user_review first_review'}) :
-            f_r = self.get_special_review(soup.find('li',{'class':'review user_review first_review'}))
-            self.user_reviews[f_r[0]] = {'name':f_r[1], 'review':f_r[2], 'score':f_r[3]}
+            f_r = self.get_special_review(soup.find('li',{'class':'review user_review first_review'}), reviews)
+            if reviews:
+                self.user_reviews[f_r[0]] = {'name':f_r[1], 'review':f_r[2], 'score':f_r[3]}
+            else :
+                self.user_reviews[f_r[0]] = {'name':f_r[1], 'score':f_r[2]}
         #If statement to check if there are at laest two 
         if soup.find('li',{'class':'review user_review last_review'}): 
-            l_r = self.get_special_review(soup.find('li',{'class':'review user_review last_review'}))
-            self.user_reviews[l_r[0]] = {'name':l_r[1], 'review':l_r[2], 'score':l_r[3]}
+            l_r = self.get_special_review(soup.find('li',{'class':'review user_review last_review'}), reviews)
+            if reviews:
+                self.user_reviews[l_r[0]] = {'name':l_r[1], 'review':l_r[2], 'score':l_r[3]}
+            else:
+                self.user_reviews[l_r[0]] = {'name':l_r[1], 'score':l_r[2]}
         #If statements to check if there is only one review
-        if soup.find('li',{'class':'review user_review first_review last_review'}):
+        elif soup.find('li',{'class':'review user_review first_review last_review'}):
             only_review = self.parse_tag(
-                    soup.find('li',{'class':'review user_review first_review last_review'}))
-            self.user_reviews[only_review[0]] = {'name':only_review[1],
-                        'review':only_review[2],'score':only_review[2]}
+                    soup.find('li',{'class':'review user_review first_review last_review'}), reviews)
+            if reviews:
+                self.user_reviews[only_review[0]] = {'name':only_review[1],
+                         'review':only_review[2],'score':only_review[3]}
+            else:
+                self.user_reviews[only_review[0]] = {'name':only_review[1],
+                                     'score':only_review[2]}
         # necessary if statement in case there are less than 3 reviews
         if user_elements: 
             for user_review in user_elements:
-                data = self.parse_tag(user_review)
-                self.user_reviews[user_review['id']] = {'name':data[0],
-                            'review':data[1], 'score':int(data[2])}
+                data = self.parse_tag(user_review, reviews)
+                if reviews:
+                    self.user_reviews[user_review['id']] = {'name':data[0],
+                                'review':data[1], 'score':int(data[2])}
+                else:
+                    self.user_reviews[user_review['id']] = {'name':data[0],
+                                     'score':int(data[1])}
         # if pool = 1 and there are more reviews on a different webpage:
         #         get all the user reviews for next page    
         if pool and soup.find('a', {'class':'action','rel':'next'}, href=True):
-            self.get_user_reviews(url, pool, page+1)
+            self.get_user_reviews(url, pool, reviews, page+1)
         return self.user_reviews
 
     # get critic rerviews
-    def get_critic_reviews(self, url, pool, page=0):
+    def get_critic_reviews(self, url, pool, reviews, page=0):
         req = Request(url + '/critic-reviews?page=' + str(page)
                       , headers={'user-Agent': 'Mozilla/5.0'})
         html_doc = urlopen(req).read()
         soup = BeautifulSoup(html_doc,'lxml')
-        ol = soup.find_all('ol',{'class':'reviews critic_reviews'})
+        ol = soup.find('ol',{'class':'reviews critic_reviews'})
         critic_elements = 0
         if ol:
-            critic_elements = ol[0].find_all('li',{'class':'review critic_review'})
+            critic_elements = ol.find_all('li',{'class':'review critic_review'})
             #If statement to check if there is at least one review
         if soup.find('li',{'class':'review critic_review first_review'}):
-            f_r = self.get_special_review(soup.find('li',{'class':'review critic_review first_review'}), False)
-            self.critic_reviews[f_r[0]] = {'review':f_r[1], 'score':f_r[2]}
+            f_r = self.get_special_review(soup.find('li',{'class':'review critic_review first_review'}), reviews, False)
+            if reviews:
+                self.critic_reviews[f_r[0]] = {'review':f_r[1], 'score':f_r[2]}
+            else:
+                self.critic_reviews[f_r[0]] = {'score':f_r[1]}
         #If statement to check if there are at laest two 
         if soup.find('li',{'class':'review critic_review last_review'}):
-            l_r = self.get_special_review(soup.find('li',{'class':'review critic_review last_review'}), False)
-            self.critic_reviews[l_r[0]] = {'review':l_r[1], 'score':l_r[2]}
+            l_r = self.get_special_review(soup.find('li',{'class':'review critic_review last_review'}), reviews, False)
+            if reviews:
+                self.critic_reviews[l_r[0]] = {'review':l_r[1], 'score':l_r[2]}
+            else:
+                self.critic_reviews[l_r[0]] = {'score':l_r[1]}
         #If statement to check if there is only one review
-        if soup.find('li',{'class':'review critic_review first_review last_review'}):
+        elif soup.find('li',{'class':'review critic_review first_review last_review'}):
             only_review = self.parse_tag(
-                    soup.find('li',{'class':'review critic_review first_review last_review'}))
-            self.critic_reviews[only_review[0]] = {'review':only_review[1]
-                                    ,'score':only_review[2]}
+                    soup.find('li',{'class':'review critic_review first_review last_review'}), reviews)
+            if reviews:
+                self.critic_reviews[only_review[0]] = {'review':only_review[1],
+                                        'score':only_review[2]}
+            else:
+                self.critic_reviews[only_review[0]] = {'score':only_review[1]}
         #If statement in case there are more than 2 reviews
         if critic_elements:
             for critic_review in critic_elements:
-                data = self.parse_tag(critic_review)
-                self.critic_reviews[data[0]] = {'review':data[1], 'score':data[2]}
+                data = self.parse_tag(critic_review, reviews)
+                if reviews:
+                    self.critic_reviews[data[0]] = {'review':data[1], 'score':data[2]}
+                else:
+                    self.critic_reviews[data[0]] = {'score':data[1]}
         # if pool = 1 and there are more reviews on a different webpage:
         #         get all the user reviews for next page
         if pool and soup.find('a', {'class':'action','rel':'next'}, href=True):
-            self.get_user_reviews(url, pool, page+1)
+            self.get_user_reviews(url, pool, reviews, page+1)
         return self.critic_reviews
 
     #extrace li element literals
-    def parse_tag(self, tag_data):
+    def parse_tag(self, tag_data, reviews):
+        if reviews:
+            return (
+                    tag_data.find('a',href=True).getText(),
+                    tag_data.find('div',{'class':'review_body'}).getText(),
+                    tag_data.find('div',{'class':'metascore_w'}).getText())                    
         return (
                 tag_data.find('a',href=True).getText(),
-                tag_data.find('div',{'class':'review_body'}).getText(),
                 tag_data.find('div',{'class':'metascore_w'}).getText())
 
     #extract first and last review 
-    def get_special_review(self, tag, user=True):
+    def get_special_review(self, tag, reviews, user=True):
         if user:
-            tag_data = self.parse_tag(tag)
-            return(tag['id'],tag_data[0],tag_data[1],
-                   int(tag_data[2]))
+            tag_data = self.parse_tag(tag, reviews)
+            if reviews:
+                return(tag['id'],tag_data[0],tag_data[1],
+                       tag_data[2])
+            return(tag['id'],tag_data[0],tag_data[1])
         else:
-            tag_data = self.parse_tag(tag)
-            return(tag_data[0],tag_data[1],
-                   int(tag_data[2]))
+            tag_data = self.parse_tag(tag, reviews)
+            if reviews:
+                return(tag_data[0],tag_data[1],
+                       tag_data[2])
+            return(tag_data[0],tag_data[1])
+    
+    #Calculate averages for user and critic reviews. 
+    def calculateAvgs(self):
+        user_avg = sum([int(self.user_reviews[k]['score']) 
+                        for k in self.user_reviews])/len(self.user_reviews)
+        self.user_reviews['average'] = round(user_avg,1)
+        crit_avg = sum([int(self.critic_reviews[k]['score']) 
+                        for k in self.critic_reviews])/len(self.critic_reviews)
+        self.critic_reviews['average'] = round(crit_avg)
 
 if __name__ == '__main__':
     #set pool to 1 to test if script pulls all reviews.
