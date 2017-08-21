d646f53704d4e632f476d9c4ce2a7a335707f979
Derling <derling0704@gmail.com>
2017-08-21 08:31:41 -0400

    Initialized class model

--- MetaGraphs.py
+++ MetaGraphs.py
@@ -1,13 +1,31 @@
+#!/usr/bin/env python3
 from metacritic import PyGameCritic as pgn
-import logging 
-import matplotlib.pyplot as plt
+import logging
+import matplotlib.pyplot as plt 
+
 
 #Module for generating bar graphs comparing the metacritic scores of a game
 #accross multiple consoles. 
-class MetaGraphs(plt):
-    def __init(self):
-        #game title as param,
-        #try to get the docs for the consoles that the game was 
-        # released on, if not in a certain console, pass(do nothing)
-        pass
-    
+
+#Current generation of consoles, also only ones supported on metacritic.com
+CONSOLES = ['playstation-4','xbox-one','pc','switch','wii-u','3ds',
+				'playstation-vita','ios','legacy']
+
+class MetaGraphs():
+    def __init__(self,game):
+        self.data = []
+        self.game = game
+        self.find_consoles()
+
+    def find_consoles(self):
+    	for console in CONSOLES:
+    		try: # try all consoles to find the consoles that the game was
+    			 # released on.
+    			tmp = pgn.PyGameCritic(console,self.game)
+    			self.data.append(tmp)
+    		except Exception as e:
+    			#If the game is not available on a certain console, do nothing.
+    			pass
+
+if __name__ == '__main__':
+	x = MetaGraphs('the witcher 3 wild hunt')  
