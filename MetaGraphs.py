4bf50cfbc5fa1be0e39d2119c23dcf1321f90f1a
Derling Batista <derling0704@gmail.com>
2017-08-20 23:29:29 -0400

    Initializing metagraphs class

--- MetaGraphs.py
+++ MetaGraphs.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 
 #Module for generating bar graphs comparing the metacritic scores of a game
 #accross multiple consoles. 
-class MetaGraphs(plt):#extend pyplot 
-    pass
+class MetaGraphs(plt):
+    def __init(self):
+        #game title as param,
+        #try to get the docs for the consoles that the game was 
+        # released on, if not in a certain console, pass(do nothing)
+        pass
     
