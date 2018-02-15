15412294aff77b0c3323200af7f52f44a0690496
Derling <derling0704@gmail.com>
2018-02-15 12:10:26 -0500

    Updated parsing algorithm

--- metacritic/pygamecritic.py
+++ metacritic/pygamecritic.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from collections import namedtuple, Counter
 
-''' 
-    To only grab critic reviews enter users=False as a parameter
-    To only grab user reviews enter critics=False as a parameter
-    To only pull one page worth of reviews enter pool=False
-    To grab the content of all reviews enter reviews=True
-''' 
+'''
+    critics field used to determine whether to pull critic data or not
+    users field used to determine whether to pull user data or not
 
+'''
+
+MetaData = namedtuple('MetaData', ['review', 'score',])
 
 class PyGameCritic():
-    
     def __init__(self, console, game, critics=True, users=True, pool=True, reviews=False):
         self.user_reviews = {}
         self.critic_reviews = {}
@@ -21,90 +21,60 @@ class PyGameCritic():
         self.console = console
         self.game = game
         self.reviews = reviews
-        self.get_all_metacritic_data()
-        self.calculateAvgs()
-        self.userTotals()
-        self.criticTotals()
-        
-    def get_all_metacritic_data(self): 
+        self.__get_all_metacritic_data()
+
+    def __get_all_metacritic_data(self): 
         url = 'http://www.metacritic.com/game/{0}/{1}'.format(
                     self.console.lower().replace(' ','-'),
                     self.game.lower().replace(' ','-')
                     )
         if self.critics: 
-            self.get_critic_reviews(url)
+            self.__get_critic_reviews(url)
         if self.users: 
-            self.get_user_reviews(url)
+            self.__get_user_reviews(url)
     
-    def get_user_reviews(self, url, page=0):
+    def __get_user_reviews(self, url, page=0):
         req = Request(
                 url + '/user-reviews?page=' + str(page),
                 headers={'User-Agent': 'Mozilla/5.0'}
                 )
+
         html_doc = urlopen(req).read()
         soup = BeautifulSoup(html_doc,'lxml')
         ol = soup.find('ol', {'class':'reviews user_reviews'})
-        user_elements = 0
+
+        user_reviews = None
         if ol:
-            user_elements = ol.find_all('li',{'class':'review user_review'})
-        elif soup.find('li',{'class':'review user_review first_review'}) :
-            f_r = self.get_special_review(
-                  soup.find('li', {'class':'review user_review first_review'}))
-            if self.reviews:
-                self.user_reviews[f_r[0]] = {
-                        'name':f_r[1], 
-                        'review':f_r[2], 
-                        'score':f_r[3]
-                        }
-            else :
-                self.user_reviews[f_r[0]] = {
-                        'name':f_r[1], 
-                        'score':f_r[2]
-                        }
-        elif soup.find('li', {'class':'review user_review last_review'}): 
-            l_r = self.get_special_review(
-                  soup.find('li', {'class':'review user_review last_review'}))
-            if self.reviews:
-                self.user_reviews[l_r[0]] = {
-                        'name':l_r[1], 
-                        'review':l_r[2], 
-                        'score':l_r[3]
-                        }
-            else:
-                self.user_reviews[l_r[0]] = {
-                        'name':l_r[1], 
-                        'score':l_r[2]
-                        }
-        elif soup.find(
-             'li', {'class':'review user_review first_review last_review'}):
-            only_review = self.parse_tag(soup.find(
+            user_reviews = ol.find_all('li',{'class':'review user_review'})
+
+        if soup.find(
+             'li', {'class':'review user_review first_review last_review'}): # only one review on current page
+            name, review_data = self.__parse_tag(soup.find(
                 'li', {'class':'review user_review first_review last_review'}))
-            if self.reviews:
-                self.user_reviews[only_review[0]] = {
-                        'name':only_review[1],
-                        'review':only_review[2],
-                        'score':only_review[3]
-                        }
-            else:
-                self.user_reviews[only_review[0]] = {
-                        'name':only_review[1],
-                        'score':only_review[2]
-                        }
-        if user_elements: 
-            for user_review in user_elements:
-                data = self.parse_tag(user_review)
-                if self.reviews:
-                    self.user_reviews[user_review['id']] = {'name':data[0],
-                                'review':data[1], 'score':int(data[2])}
-                else:
-                    self.user_reviews[user_review['id']] = {'name':data[0],
-                                     'score':int(data[1])}
+            self.user_reviews[name] = {k: v for k, v in review_data._asdict().items() if v != None}
+
+
+        if soup.find('li',{'class':'review user_review first_review'}): # get first review on current page
+            name, review_data = self.__parse_tag(
+                  soup.find('li', {'class':'review user_review first_review'}))
+            self.user_reviews[name] = {k: v for k, v in review_data._asdict().items() if v != None}
+
+        if user_reviews: # reviews between first and last review
+            for user_review in user_reviews:
+                name, review_data = self.__parse_tag(user_review)
+                self.user_reviews[name] = {k: v for k, v in review_data._asdict().items() if v != None}
+
+        if soup.find('li', {'class':'review user_review last_review'}): # get last review on current page
+            name, review_data = self.__parse_tag(
+                  soup.find('li', {'class':'review user_review last_review'}))
+            self.user_reviews[name] = {k: v for k, v in review_data._asdict().items() if v != None}
+
         if (self.pool and 
                 soup.find('a', {'class':'action','rel':'next'}, href=True)):
-            self.get_user_reviews(url, page+1) # recursevely get all reviews
-        return self.user_reviews
+            self.get_user_reviews(url, page+1) # if pool recursively get all reviews from following page
 
-    def get_critic_reviews(self, url, page=0):
+
+    def __get_critic_reviews(self, url, page=0):
         req = Request(
                 url + '/critic-reviews?page=' + str(page),
                 headers={'user-Agent': 'Mozilla/5.0'}
@@ -112,157 +82,82 @@ class PyGameCritic():
         html_doc = urlopen(req).read()
         soup = BeautifulSoup(html_doc,'lxml')
         ol = soup.find('ol',{'class':'reviews critic_reviews'})
-        critic_elements = 0
+        critic_elements = None
+
         if ol:
             critic_elements = ol.find_all('li', {'class':'review critic_review'})
-        elif soup.find('li',{'class':'review critic_review first_review'}): 
-            f_r = self.get_special_review(soup.find(
-                  'li', {'class':'review critic_review first_review'}), False)
-            if self.reviews:
-                self.critic_reviews[f_r[0]] = {
-                        'review':f_r[1], 
-                        'score':f_r[2]
-                        }
-            else:
-                self.critic_reviews[f_r[0]] = {
-                        'score':f_r[1]
-                        }
-        elif soup.find('li',{'class':'review critic_review last_review'}):
-            l_r = self.get_special_review(soup.find(
-                  'li', {'class':'review critic_review last_review'}), False)
-            if self.reviews:
-                self.critic_reviews[l_r[0]] = {
-                        'review':l_r[1], 
-                        'score':l_r[2]
-                        }
-            else:
-                self.critic_reviews[l_r[0]] = {
-                        'score':l_r[1]
-                        }
-        elif soup.find(
-              'li', {'class':'review critic_review first_review last_review'}):
-            only_review = self.parse_tag(soup.find(
-                    'li', {'class':'review critic_review first_review last_review'}))
-            if self.reviews:
-                self.critic_reviews[only_review[0]] = {
-                        'review':only_review[1],
-                        'score':only_review[2]
-                        }
-            else:
-                self.critic_reviews[only_review[0]] = {
-                        'score':only_review[1]
-                        }
-        if critic_elements:
+
+        if soup.find(
+              'li', {'class':'review critic_review first_review last_review'}): # only one review on current page
+        	name, review_data = self.__parse_tag(soup.find(
+        		'li', {'class':'review critic_review first_review last_review'}))
+        	self.critic_reviews[name] = {k: v for k, v in review_data._asdict().items() if v != None}
+
+        if soup.find('li',{'class':'review critic_review first_review'}): # get first review on current page
+            name, review_data = self.__parse_tag(soup.find(
+        		'li', {'class':'review critic_review first_review'}))
+            self.critic_reviews[name] = {k: v for k, v in review_data._asdict().items() if v != None}
+
+        
+        if critic_elements: # 
             for critic_review in critic_elements:
-                data = self.parse_tag(critic_review)
-                if self.reviews:
-                    self.critic_reviews[data[0]] = {
-                            'review':data[1], 
-                            'score':data[2]
-                            }
-                else:
-                    self.critic_reviews[data[0]] = {
-                            'score':data[1]
-                            }
+            	name, review_data = self.__parse_tag(critic_review)
+            	self.critic_reviews[name] = {k: v for k, v in review_data._asdict().items() if v}
+
+        if soup.find('li',{'class':'review critic_review last_review'}): # get last review on current page
+        	name, review_data = self.__parse_tag(soup.find(
+        		'li', {'class':'review critic_review last_review'}))
+        	self.critic_reviews[name] = {k: v for k, v in review_data._asdict().items() if v != None}
+
+        
+            
         if (self.pool and 
             soup.find('a', {'class':'action','rel':'next'}, href=True)):
             self.get_user_reviews(url, page+1) # recursively get all reviews
         return self.critic_reviews
 
-    def parse_tag(self, tag_data): # get revelant data based on review boolean
-        if self.reviews:
-            return (
-                tag_data.find('a',href=True).getText(),
-                tag_data.find('div',{'class':'review_body'}).getText(),
-                tag_data.find('div',{'class':'metascore_w'}).getText()
-                    )                    
-        return (
-            tag_data.find('a',href=True).getText(),
-            tag_data.find('div',{'class':'metascore_w'}).getText()
-                )
+    def get_user_scores(self):
+    	# return the number of times the game was given a certain score by users
+    	if self.users:
+    		return Counter([self.user_reviews[name]['score'] for name in self.user_reviews])
+    	return None
 
-    def get_special_review(self, tag, user=True): # case for 1 or 2 reviews
-        if user:
-            tag_data = self.parse_tag(tag)
-            if self.reviews:
-                return(
-                    tag['id'],tag_data[0],
-                    tag_data[1],
-                    tag_data[2]
-                    )
-            return(
-                tag['id'],
-                tag_data[0],
-                tag_data[1]
-                )
-        else:
-            tag_data = self.parse_tag(tag)
-            if self.reviews:
-                return(
-                    tag_data[0],
-                    tag_data[1],
-                    tag_data[2]
-                    )
-            return(
-                tag_data[0],
-                tag_data[1]
-                )
-    
-    def calculateAvgs(self):
-        user_avg = sum([int(self.user_reviews[k]['score']) 
-                   for k in self.user_reviews]) / len(self.user_reviews)
-        self.user_reviews['average'] = round(user_avg,1)
-        crit_avg = sum([int(self.critic_reviews[k]['score']) 
-                   for k in self.critic_reviews]) / len(self.critic_reviews)
-        self.critic_reviews['average'] = round(crit_avg)
-    
-    def userTotals(self):
-        self.user_reviews['totals'] = {}
-        self.user_reviews['summary'] = {}
-        #0-4 negative(red), 5-7 mixed(yellow),8-10 positive(green) 
-        for _id in self.user_reviews :
-            #Skip the average, totals and summary keys - they are not reviews
-            if _id == 'average' or _id == 'totals' or _id == 'summary':
-                continue                           
-            rating = int(self.user_reviews[_id]['score'])
-            self.user_reviews['totals'][rating] = \
-                self.user_reviews['totals'].get(rating, 0) + 1
-            if rating < 5 : 
-                self.user_reviews['summary']['negative_reviews'] = \
-                self.user_reviews['summary'].get('negative_reviews', 0) + 1
-            elif rating < 8 :
-                self.user_reviews['summary']['mixed_reviews'] = \
-                self.user_reviews['summary'].get('mixed_reviews', 0) + 1
-            else :
-                self.user_reviews['summary']['positive_reviews'] = \
-                self.user_reviews['summary'].get('positive_reviews', 0) + 1
-            self.user_reviews['summary']['num_of_reviews'] = \
-                self.user_reviews['summary'].get('num_of_reviews', 0) + 1
-    
-    def criticTotals(self):
-        self.critic_reviews['totals'] = {}
-        self.critic_reviews['summary'] = {}
-        #0-49 negative(red),50-74 mixed(yellow), 75-100 positive(green)
-        for _id in self.critic_reviews:
-            #Skip the average, totals and summary keys - they are not reviews
-            if _id == 'average' or _id == 'totals' or _id == 'summary':
-                continue                           
-            rating = int(self.critic_reviews[_id]['score'])
-            self.critic_reviews['totals'][rating] = \
-                self.critic_reviews['totals'].get(rating, 0) + 1
-            if rating < 50:
-                self.critic_reviews['summary']['negative_reviews'] = \
-                self.critic_reviews['summary'].get('negative_reviews', 0) + 1
-            elif rating < 75:
-                self.critic_reviews['summary']['mixed_reviews'] = \
-                self.critic_reviews['summary'].get('mixed_reviews', 0) + 1
-            else: 
-                 self.critic_reviews['summary']['positive_reviews'] = \
-                 self.critic_reviews['summary'].get('positive_reviews', 0) + 1
-            self.critic_reviews['summary']['num_of_reviews'] = \
-                self.critic_reviews['summary'].get('num_of_reviews', 0) + 1
+    def get_user_average(self):
+    	# return the average of all the user scores
+    	if self.users:
+    		n = total = 0
+    		for name in self.user_reviews:
+    			total += int(self.user_reviews[name]['score'])
+    			n += 1
+    		return round(total/n, 1) # round to the tenth digit
+    	return None
+
+    def get_critic_scores(self):
+    	# return the number of times the game was given a certain score by critics
+    	if self.ritics:
+    		return Counter([self.critic_reviews[name]['score'] for name in self.user_reviews])
+    	return None
+
+    def get_critic_average(self):
+    	# return the average of all the critic scores
+    	if self.critics:
+    		n = total = 0
+    		for name in self.critic_reviews:
+    			total += int(self.critic_reviews[name]['score'])
+    			n += 1
+    		return round(total/n, 1) # round to the tenth digit
+    	return None
+
+    def __parse_tag(self, tag):
+    	# extracts critic or user name and their review(if needed) and score
+    	name = tag.find('a',href=True).getText()
+    	return name, MetaData(
+    		review=tag.find('div',{'class':'review_body'}).getText() if self.reviews else None,
+        	score=tag.find('div',{'class':'metascore_w'}).getText(),
+        )
                 
     def __repr__(self):
+    	# class instance representation
         return 'PyGameCritic("{0}", "{1}", critics={2},users={3}, pool={4},'\
                 ' reviews={5})'.format(
                             self.console,
@@ -274,19 +169,15 @@ class PyGameCritic():
                             )
     
     def __str__(self):
-        return '{0} for {1} has {2} user reviews and {3} critic reviews'\
-                .format(
-                self.game, 
-                self.console, 
-                self.user_reviews['summary']['num_of_reviews'],
-                self.critic_reviews['summary']['num_of_reviews']
-                )
+        # NEED TO CREATE __str__ method
+        return ''
         
 if __name__ == '__main__':
     try:
-        PyGameCritic(
+        x = PyGameCritic(
                 input('Enter the console:').strip(),
-                input('Enter the game:').strip()
+                input('Enter the game:').strip(), 
+            	pool=False, reviews=True
                 )
     except Exception as e:
         raise Exception(
